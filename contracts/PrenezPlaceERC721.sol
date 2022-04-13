ed50666e71bea4280a7077473f62d06b897793f7
PhilippeMts <phi.metais@gmail.com>
2022-04-13 07:32:49 +0200

    📝 Add or update documentation.

--- contracts/PrenezPlaceERC721.sol
+++ contracts/PrenezPlaceERC721.sol
@@ -5,7 +5,8 @@ pragma solidity ^0.8.0;
 import "./ERC721Tradable.sol";
 
 /**
- * @title Prenez Place
+ * @title Creature
+ * Creature - a contract for my non-fungible creatures.
  */
 contract PrenezPlaceERC721 is ERC721Tradable {
     constructor(address _proxyRegistryAddress)
